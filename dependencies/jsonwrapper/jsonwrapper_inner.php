bc888f96a2cc68c3d244a4a7a7818e20780bef54
Daniel Marschall <info@daniel-marschall.de>
2023-09-15 23:27:59 +0000

    Fixed problem with PHP linter
    git-svn-id: https://svn.viathinksoft.com/svn/cryptochat/trunk@8 97914aee-75f3-442a-8326-69c5ee27cdba

--- dependencies/jsonwrapper/jsonwrapper_inner.php
+++ dependencies/jsonwrapper/jsonwrapper_inner.php
@@ -2,22 +2,22 @@
 
 require_once 'JSON/JSON.php';
 
-function json_encode($arg)
-{
-	global $services_json;
-	if (!isset($services_json)) {
-		$services_json = new Services_JSON();
+if (!function_exists('json_encode')) {
+	function json_encode($arg) {
+		global $services_json;
+		if (!isset($services_json)) {
+			$services_json = new Services_JSON();
+		}
+		return $services_json->encode($arg);
 	}
-	return $services_json->encode($arg);
 }
 
-function json_decode($arg)
-{
-	global $services_json;
-	if (!isset($services_json)) {
-		$services_json = new Services_JSON();
+if (!function_exists('json_decode')) {
+	function json_decode($arg) {
+		global $services_json;
+		if (!isset($services_json)) {
+			$services_json = new Services_JSON();
+		}
+		return $services_json->decode($arg);
 	}
-	return $services_json->decode($arg);
 }
-
-?>
